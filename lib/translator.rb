85c479f3edb66ac133d1bdc2cf3d376e4459b445
Alex Farmer <farmale10@gmail.com>
2019-09-03 17:25:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,7 @@ def load_library(file)
     emoji_hash["get_meaning"][emoticons[1]] = meaning
     emoji_hash["get_emoticon"][emoticons[0]] = emoticons[1]
   end
+end
   emoji_hash
 
 def get_japanese_emoticon
